d6c60e360e359fba8cdc02bdafb44a0edcad1cc2
Anindra <anindrakarmakar+git@proton.me>
2024-09-15 12:45:14 +0530

    Add tanstack

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import biomePlugin from "vite-plugin-biome";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import biomePlugin from 'vite-plugin-biome'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), biomePlugin({})],
-});
+  plugins: [react(), biomePlugin(), TanStackRouterVite(), viteReact()],
+})
